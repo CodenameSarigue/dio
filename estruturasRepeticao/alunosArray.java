64350923bb237c660c3f7f0f9287292a0cf8daca
CodenameSarigue <igorsampaio@outlook.com>
2022-10-11 01:30:26 -0300

    adicionando exercícios do bootcamp

---
+++ estruturasRepeticao/alunosArray.java
@@ -0,0 +1,11 @@
+package DIO.estruturasRepeticao;
+
+public class alunosArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: "+aluno);
+        }
+    }
+}
---
+++ estruturasRepeticao/estruturasRepeticao.java
@@ -0,0 +1,10 @@
+package DIO.estruturasRepeticao;
+
+public class estruturasRepeticao {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos <=20; carneirinhos ++){
+            System.out.println("Contando carneirinhos: "+carneirinhos);
+        }
+        System.out.println("Joãozinho dormiu");
+    }
+}
